d9af670ed956e0c741e0b7700c3f01941281ec0e
Ahmedul Haque Abid <a_h_abid@hotmail.com>
2025-04-09 01:51:20 +0600

    Update Auth API for expires time

--- auth-app/routes/api.php
+++ auth-app/routes/api.php
@@ -21,31 +21,37 @@ Route::post('/auth/login', function (Request $request) {
     $username = $request->get('username');
     $password = $request->get('password');
 
+    // Dummy Password
     if ($password != '1234') {
         return response()->json([
             'message' => 'Invalid User',
         ], 401);
     }
 
+    $validTokenForSeconds = 120;
+    $token = Str::random(32);
+    $expires = now()->addSeconds($validTokenForSeconds);
     $userData = [
         'id' => md5($username),
         'username' => $username,
         'is_active' => 1,
     ];
-    $userEncoded = base64_encode(json_encode($userData));
-
-    $token = Str::random(32);
-    $expires = now()->addMinute();
+    $userEncoded = base64_encode(
+        json_encode($userData + ['expires_ts' => $expires->timestamp])
+    );
 
     Cache::put($token, $userEncoded, $expires);
 
-    Log::info("[$token] /auth/login Token Cached");
-
-    return response()->json([
+    $responseData = [
         'token' => $token,
-        'expires' => $expires->timestamp,
+        'expires_in_s' => $validTokenForSeconds,
+        'expires_ts' => $expires->timestamp,
         'data' => $userData,
-    ]);
+    ];
+
+    Log::info("[$token] /auth/login Token Cached", $responseData);
+
+    return response()->json($responseData);
 });
 
 Route::get('/auth/verify', function (Request $request) {
@@ -73,8 +79,22 @@ Route::get('/auth/verify', function (Request $request) {
     Log::info("[$token] /auth/verify User Found");
 
     $userData = json_decode(base64_decode($userData64), true);
+    if (!$userData) {
+        Log::info("[$token] /auth/verify User Data Invalid");
+        return response()->json([
+            'message' => 'User token is broken',
+        ], 401);
+    }
+
+    $expiresTs = time() + 60; // This 60s cache remember for could come from config.
+    if (isset($userData['expires_ts'])) {
+        $expiresTs = $userData['expires_ts'];
+        unset($userData['expires_ts']);
+    }
 
     return response()->json([
         'data' => $userData,
+    ], 200, [
+        'X-Token-Expires-Ts' => $expiresTs,
     ]);
 });
